a0ff48e0698845e00a659720eaded0af5a679eee
RbVs <r.b.vs@outlook.de>
2020-09-28 00:38:15 +0200

    30. Invoking JavaScript Functions From C#

---
+++ Tutorial.BlazorMovies/Client/Helpers/IJsRuntimeExtentionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Tutorial.BlazorMovies.Client.Helpers
+{
+    public static class IJsRuntimeExtentionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("console.log", "example message");
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
+        }
+    }
+}
